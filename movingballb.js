77a222d8ca365d18ff6d33c5336cd352198fa196
bleungwpg <benjamin.leung80@gmail.com>
2016-01-27 09:49:27 +0800

    v8

--- movingballb.js
+++ movingballb.js
@@ -2,8 +2,7 @@ function setup()
 {
 
 	createCanvas(600, 600);
-	x = 20;
-	y = 20;
+	x = 20;  // create a variable x with a starting value of 20
 	
 }  
 
@@ -12,6 +11,7 @@ function draw()
 {
 	background(255,255,255);
 
-	ellipse(x,50,20,20);
-x = x + 1;
+	ellipse(x,100,20,20);
+	
+	x = x + 1;  // increase the value of x+1 each time
 }
