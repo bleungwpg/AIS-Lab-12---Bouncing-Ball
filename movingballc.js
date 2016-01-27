0b93691f4c629a38142748438936fa8035df31ed
bleungwpg <benjamin.leung80@gmail.com>
2016-01-27 09:47:15 +0800

    v6

--- movingballc.js
+++ movingballc.js
@@ -1,23 +1,43 @@
 function setup()
 {
 
-	createCanvas(600, 600);
-	x = 20;
-	y = 20;
-	
+	createCanvas(600, 400);
+	x = 250;
+	y = 21;
+
+	moveR = 1;
+	// moveR = 1 means moving right
+	// moveR = 0 means moving left
 }  
 
 
 function draw()
 {
-	background(255,255,255);
+	background(125,125,125);
 
 	fill(255,0,0);
 	ellipse(x,y,20,20);
-	x = x + 10;
 
-	if (x > 450)
+	
+	if (moveR == 1)
 	{
-		x = x - 17;
+		x = x + 4;
 	}
+	if (moveR == 0)
+	{
+		x = x - 5;
+	}
+
+	// when the x value of the ball reaches a maximum
+	if (x > 590)
+	{
+		moveR = 0;
+	}
+	// when the x value of the ball reaches a minimum
+	if (x < 10)
+	{
+		moveR = 1;
+	}
+
+
 }
